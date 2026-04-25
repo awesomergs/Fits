bccbaf73219454a98103321ead8857daab04e4ce
awesomergs <90956516+awesomergs@users.noreply.github.com>
2026-04-25 15:55:22 -0700

    Initial Commit

---
+++ Fits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fits
+//
+//  Created by Rohan George on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fits/FitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitsApp.swift
+//  Fits
+//
+//  Created by Rohan George on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
